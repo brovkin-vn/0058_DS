bdd1be56d20013a2d2412a337e30a5e17b58ce4e
brovkin-vn <v-brovkin@yandex.ru>
2021-02-24 15:37:52 +0700

    202102201537

--- module_2/test_21.py
+++ module_2/test_21.py
@@ -2,7 +2,16 @@ import pandas as pd
 from IPython.display import display
 
 df = pd.read_csv('../module_1/data_sf.csv')
-
 small_df = df[df.columns[1:8]].head(25)
-s = small_df['Nationality'].value_counts(normalize=True)
-display(s)
+
+# s = df['Position'].value_counts(normalize=True)
+# display(s)
+# s = small_df['Age'].value_counts(bins=3)
+# display(s)
+# s = small_df['Wage'].value_counts(bins=4)
+# display(s)
+
+# display(small_df.loc[(small_df['Wage'] > s.index[3].left) & (small_df['Wage'] <= s.index[3].right)])
+
+s = df['FKAccuracy'].value_counts(bins=5)
+print(s)
---
+++ module_2/test_22.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from IPython.display import display
+
+df = pd.read_csv('../module_1/data_sf.csv')
+small_df = df[df.columns[1:8]].head(25)
+
+# s = small_df['Nationality'].value_counts()
+# print(s.index)
+# print(len(s.index))
+
+# print(small_df['Nationality'].unique())
+# print(len(small_df['Nationality'].unique()))
+
+# result = len(df['Position'].unique)
+# result = df['Position'].nunique()
+# print(1, result)
+# result = len(df['Position'].unique())
+# print(2, result)
+# result = df['Position'].unique()
+# print(3, result)
+# result = df['Position'].count()
+# print(4, result)
+# result = len(df['Position'].value_counts())
+# print(5, result)
+
+s = small_df['Nationality'].value_counts()
+s_df = s.reset_index()
+s_df.columns = ['Nationality','Players Count']
+display(s_df)
+
