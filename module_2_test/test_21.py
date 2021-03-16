562eae50481d350837f51fccfb6078d9efc93393
Viktor.Brovkin@evraz.com <Brovkin_VN@uku.evraz.com>
2021-03-16 10:48:39 +0700

    202103161048

--- module_2_test/test_21.py
+++ module_2_test/test_21.py
@@ -125,12 +125,12 @@ import numpy as np
 # print(np.sort(my_array, axis=1))
 # print(np.sort(my_array, axis=0))
 
-first = [x**(1/2) for x in range(100)]
-second = [x**(1/3) for x in range(100, 200)]
-third = [x/y for x in range(200,300,2) for y in [3,5]]
+# first = [x**(1/2) for x in range(100)]
+# second = [x**(1/3) for x in range(100, 200)]
+# third = [x/y for x in range(200,300,2) for y in [3,5]]
 
-great_secret = np.array([first, second, third]).T
-print(great_secret)
+# great_secret = np.array([first, second, third]).T
+# print(great_secret)
 
 # Чему равна сумма косинусов элементов первой строки массива great_secret? Ответ округлите до двух знаков после запятой.
 # a = great_secret[0:1]
@@ -158,5 +158,92 @@ print(great_secret)
 # print(a)
 # print(round(b.sum(),2))
 
+students = np.array([
+    [1, 135, 34, 4],
+    [2, 160, 43, 5],
+    [3, 163, 40, 4.3],
+    [4, 147, 44, 5],
+    [5, 138, 41, 4.7],
+    [6, 149, 54, 3.9],
+    [7, 136, 39, 4.2],
+    [8, 154, 48, 4.9],
+    [9, 137, 35, 3.7],
+    [10, 165, 60, 4.6]
+])
+
+# print(students)
+
+# mean = np.mean(students[:,-1])
+# print(mean)
+# median = np.median(students[:,-1])
+# print(median)
+# sort = np.sort(students[:,-1])
+# print((sort[4]+sort[5])/2)
+
+# # Чему равно медианное значение массы тела школьников?
+# print(np.median(students[:,-2]))
+# print(round(np.mean(students[:,-2]) - np.median(students[:,-2]), 1))
+
+# corr = np.corrcoef(students[:,1], students[:,2])
+# print(corr)
+
+# Между какой парой признаков в массиве students наблюдается минимальная корреляция, из предложенных вариантов?
+# # между ростом и массой тела
+# print(np.corrcoef(students[:,1], students[:,2]))
+# # между ростом и успеваемостью
+# print(np.corrcoef(students[:,1], students[:,3]))
+# # между массой тела и успеваемостью
+# print(np.corrcoef(students[:,2], students[:,3]))
+
+# std = np.std(students[:,1])
+# print(std)
+
+# Используя имеющийся набор данных о школьниках, найдите, чему равно стандартное отклонение их средних баллов.
+# std = np.std(students[:,3])
+# print(std)
+
+# Посчитайте, чему равна дисперсия значений веса школьников.
+# print(np.var(students[:,2]))
+
+# my_array = np.array([[1,2,3,4,5],
+#                      [6,7,8,9,10],
+#                      [11,12,13,14,15],
+#                      [16,17,18,19,20],
+#                      [21,22,23,24,25]])
+
+# print(my_array)                
+# # извлечь из него центральный фрагмент размером 3 х 3     
+# print(my_array[1:4,1:4])     
+# # Создайте массив my_sin, состоящий из синусов элементов массива my_array. Посчитайте, чему равна сумма элементов полученного массива. Ответ округлите до трёх цифр после запятой.                
+# my_sin = np.sin(my_array)
+# print(round(my_sin.sum() ,3))
+
+# my_sin[1:4,1:4] = 1
+# print(round(my_sin.sum() ,3))
+
+
+# Преобразуйте первые четыре столбца массива my_sin в массив из 10 строк и 2 столбцов. 
+# Чему равна сумма элементов первого столбца получившегося массива? 
+# Ответ округлите до трёх заков после точки-разделителя. 
+# Используйте преобразованный в предыдущем задании массив my_sin.
+
+
+# a = my_sin[:,0:4]
+# b = a.reshape((10,2))
+# c = b[:, 0]
+# print(a)
+# print(b)
+# print(round(c.sum(), 3))
+
+# Создайте массив bigdata, содержащий квадраты всех нечётных чисел в диапазоне от 100 до 1000.
+bigdata = np.array([x**2 for x in range(101, 1000, 2)])
+print(bigdata)
+# Чему равна медиана массива bigdata?
+print(bigdata.shape)
+print(np.median(bigdata))
+# Чему равно стандартное отклонение для массива bigdata? Ответ округлите до целых:
+print(round(np.std(bigdata)))
+# Чему равен коэффициент корреляции между элементами массива bigdata с чётными и нечётными индексами?
+print(np.corrcoef(bigdata[0::2], bigdata[1::2]))
 
 
