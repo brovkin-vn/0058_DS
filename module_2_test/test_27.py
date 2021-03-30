2ee5a9281672fc7a44fb6560a5700547e0978849
brovkin-vn <v-brovkin@yandex.ru>
2021-03-30 11:54:10 +0700

    =begin module 2

--- module_2_test/test_27.py
+++ module_2_test/test_27.py
@@ -3,6 +3,8 @@ from scipy.stats import t
 from math import sqrt
 import pandas as pd
 import math
+import numpy as np
+from statsmodels.stats import weightstats 
 
 # alpha = 0.95
 # n = 100
@@ -90,14 +92,57 @@ import math
 # p_value = (1-t.cdf(abs(Zst), k))
 # print(round(p_value, 3))
 
-n = 125
-k = n - 1
-n_ = 87
-p = 0.75
-p_ = n_/n
-print(p, p_)
-Zst = (p_ - p) / sqrt(p * (1-p)/n)
-# Zst = round(Zst, 3)
+# n = 125
+# k = n - 1
+# n_ = 87
+# p = 0.75
+# p_ = n_/n
+# print(p, p_)
+# Zst = (p_ - p) / sqrt(p * (1-p)/n)
+# # Zst = round(Zst, 3)
+# print(Zst)
+# p_value = 1-t.cdf(abs(Zst), k)
+# print(round(p_value, 3))
+# Xa, Xb  = 10.2, 16.4
+# Sa, Sb = 7.66, 9.4
+# Na, Nb = 10, 10
+# k = Na + Nb - 2
+# a = 0.01
+# Tst = (Xa - Xb) / sqrt(Sa**2 / Na +  Sb**2 / Nb)
+# print(Tst)
+# p_value = (1-t.cdf(abs(Tst), k))
+# print(round(p_value, 3))
+
+
+# print(weightstats.CompareMeans.ztest_ind(x1, x2))
+
+# import pandas as pd
+# df = pd.DataFrame({
+#     'x': [6.1, 9.2, 11.5, 8.6, 12.1, 3.9, 8.4, 10.1, 9.4, 8.9],
+#     'y': [7.3, 8.7, 12.6, 6.8, 12, 6.9, 10.2, 17.1, 9.2, 11.6]
+# })
+
+# print(weightstats.ttest_ind(df.x, df.y, usevar="unequal"))s
+
+# n1, n2 = 800, 1000
+# k = n1 + n2 - 2
+# p1, p2 = 460/n1, 520/n2
+# p = (p1*n1 + p2*n2) / (n1 + n2)
+# print(p)
+# Zst = (p1 - p2) / (sqrt(p*(1-p)) * sqrt(1/n1 + 1/n2))
+# print(Zst)
+
+# p_value = (1-t.cdf(abs(Zst), k))
+# print(round(p_value, 3))
+
+n1, n2 = 100, 100
+k = n1 + n2 - 2
+p1, p2 = 62/n1, 29/n2
+p = (p1*n1 + p2*n2) / (n1 + n2)
+print(p)
+Zst = (p1 - p2) / (sqrt(p*(1-p)) * sqrt(1/n1 + 1/n2))
 print(Zst)
-p_value = 1-t.cdf(abs(Zst), k)
+
+p_value = (1-t.cdf(abs(Zst), k))
 print(round(p_value, 3))
+
