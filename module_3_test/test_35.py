4b20eb556b13432b655bbc576caf0e9ef4db21e2
brovkin-vn <v-brovkin@yandex.ru>
2021-05-31 14:02:37 +0700

    =20210531

--- module_3_test/test_35.py
+++ module_3_test/test_35.py
@@ -10,3 +10,19 @@ def fill_net(row):
     return row.win - row.bet
 
 log['net'] = log.apply(lambda row: fill_net(row), axis=1) 
+log['winwin'] = log.net.apply(lambda x: 1 if x > 0 else 0)   
+log['is_bet'] = log.bet.apply(lambda x: 1 if x > 0 else 0)   
+
+# print(log.is_bet.count())
+# print(log.is_bet.sum())
+# print(log.bet)
+
+# print(log.loc[log.bet > 0].bet.mean())
+# print(log.loc[log.bet > 0].net.mean())
+# print(log.loc[log.net < 0].net.mean())
+# print(log.loc[log.net > 0].net.count())
+# print(log.loc[log.net < 0].net.count())
+print(log.loc[log.bet > 0].bet.min())
+print(log.loc[log.bet == log.loc[log.bet > 0].bet.min()].net.count())
+
+min_bet_amount =  log.loc[log.bet == log.loc[log.bet > 0].bet.min()].net.count()
